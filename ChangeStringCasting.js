f4c84c152e477cd898b10b7aa2f4e9e763b77f96
Charu Dwivedi <charudwivedi25@gmail.com>
2022-11-11 12:15:00 +0530

    first commit

---
+++ ChangeStringCasting.js
@@ -0,0 +1,19 @@
+var name = prompt("What is your name");
+
+//slicing first character from entire string
+
+var firstName = name.slice(0,1);
+
+//changing the first letter to uppercase
+
+var uppercaseFirstname = firstName.toUpperCase();
+
+//isolating rest of the name 
+
+var restOfname = name.slice(1,name.length);
+
+//adding both things now
+
+var capatalisedName = uppercaseFirstname + restOfname ;
+
+alert("Hello "+ capatalisedName + " !");
